4a2e224fcd03370e8879f5bda8e0bda5458ab9bc
zoff99 <zoff99@users.noreply.github.com>
2025-12-29 10:19:27 +0100

    fix asan start script

--- asan_run.sh
+++ asan_run.sh
@@ -33,19 +33,19 @@ fi
 if [ "$2""x" != "1x" ]; then
   export ASAN_OPTIONS="halt_on_error=false,detect_leaks=0"
   LD_PRELOAD="$asan_runtime_lib" \
-    "$java_17_or_newer_dir"/bin/java -Dapple.awt.application.name=TRIfA \
-    -Dcom.apple.mrj.application.apple.menu.about.name=TRIfA \
+    "$java_17_or_newer_dir"/bin/java -Dapple.awt.application.name=ToLoShare \
+    -Dcom.apple.mrj.application.apple.menu.about.name=ToLoShare \
     -Dcompose.application.configure.swing.globals=true \
     -Dcompose.application.resources.dir="$p"/build/compose/tmp/prepareAppResources \
     -Dfile.encoding=UTF-8 -Duser.country=US -Duser.language=en -Duser.variant \
-    -cp "$p"/build/compose/jars/trifa_material-linux-x64-1.0.59.jar \
-    TrifaMainKt
+    -cp "$p"/build/compose/jars/toloshare_material-linux-x64-1.0.1.jar \
+    ToLoShareMainKt
 else
-  "$java_17_or_newer_dir"/bin/java -Dapple.awt.application.name=TRIfA \
-    -Dcom.apple.mrj.application.apple.menu.about.name=TRIfA \
+  "$java_17_or_newer_dir"/bin/java -Dapple.awt.application.name=ToLoShare \
+    -Dcom.apple.mrj.application.apple.menu.about.name=ToLoShare \
     -Dcompose.application.configure.swing.globals=true \
     -Dcompose.application.resources.dir="$p"/build/compose/tmp/prepareAppResources \
     -Dfile.encoding=UTF-8 -Duser.country=US -Duser.language=en -Duser.variant \
-    -cp "$p"/build/compose/jars/trifa_material-linux-x64-1.0.59.jar \
-    TrifaMainKt
+    -cp "$p"/build/compose/jars/toloshare_material-linux-x64-1.0.1.jar \
+    ToLoShareMainKt
 fi
