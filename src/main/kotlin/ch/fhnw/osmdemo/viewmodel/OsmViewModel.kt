c52533c0c5ddd61fcc185985bca7a35e26b9e967
zoff99 <zoff99@users.noreply.github.com>
2026-01-01 16:51:41 +0100

    increase maxLevel to 21

--- src/main/kotlin/ch/fhnw/osmdemo/viewmodel/OsmViewModel.kt
+++ src/main/kotlin/ch/fhnw/osmdemo/viewmodel/OsmViewModel.kt
@@ -70,7 +70,7 @@ class OsmViewModel : ViewModel(){
     private val tapToDismissId = "Tap me to dismiss"
     private val markerColor = Color(0xCC2196F3)
 
-    private val maxLevel = 20
+    private val maxLevel = 21
     private val minLevel = 2
     private val tileSize = 256
     private val mapSize  = mapSizeAtLevel(wmtsLevel = maxLevel, tileSize = tileSize)
